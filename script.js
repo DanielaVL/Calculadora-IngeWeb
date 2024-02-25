e4a7076e64acb58cd5360155f57f641dfddaea8e
Daniela Vásquez <daniela.vasquezl1@udea.edu.co>
2024-02-25 18:26:50 -0500

    Corrección del archivo .js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Variables globales
+// Variable global
 var entradaActual = '';
-var operacionActual = '';
 
 // Función para limpiar la pantalla
 function limpiar() {
@@ -16,42 +15,61 @@ function agregarDigito(digito) {
 
 // Función para asignar la operación
 function operacion(op) {
-    operacionActual = op;
-    entradaActual += ` ${op} `;
-    actualizarDisplay(entradaActual);
+    // Verificar si ya hay una operación realizada
+    if (entradaActual !== '' && esUltimoCaracterOperador()) {
+        entradaActual = entradaActual.slice(0, -3); // Eliminar el último operador y espacio
+        actualizarDisplay(entradaActual);
+    } else {     
+        entradaActual += ` ${op} `;
+        actualizarDisplay(entradaActual);
+    }
 }
 
 // Función para calcular el resultado de la operación
 function calcularResultado() {
+    if (esUltimoCaracterOperador()) {
+        entradaActual = entradaActual.slice(0, -3); // Eliminar el último operador y espacio
+    }
+
     const operacionARealizar = entradaActual.split(' ');
     const numero1 = parseFloat(operacionARealizar[0]);
     const operador = operacionARealizar[1];
     const numero2 = parseFloat(operacionARealizar[2]);
 
-    let resultado = 0;
-    switch (operador) {
-        case '+':
-            resultado = numero1 + numero2;
-            break;
-        case '-':
-            resultado = numero1 - numero2;
-            break;
-        case '*':
-            resultado = numero1 * numero2;
-            break;
-        case '/':
-            if (numero2 === 0) {
-                resultado = 'Error';
-                alert('No se puede dividir por cero');
-            } else {
-                resultado = numero1 / numero2;
-            }
-            break;
-        default:
-            resultado = 'Error: Operación no válida';
-    }
+    if (!isNaN(numero1) && !isNaN(numero2)) {
+        let resultado = 0;
+        switch (operador) {
+            case '+':
+                resultado = numero1 + numero2;
+                break;
+            case '-':
+                resultado = numero1 - numero2;
+                break;
+            case '*':
+                resultado = numero1 * numero2;
+                break;
+            case '/':
+                if (numero2 === 0) {
+                    resultado = 'Error';
+                    alert('No se puede dividir por cero');
+                } else {
+                    resultado = numero1 / numero2;
+                }
+                break;
+            default:
+                resultado = 'Error: Operación no válida';
+        }
 
-    actualizarDisplay(resultado);
+        entradaActual = resultado.toString();
+        actualizarDisplay(resultado);
+    }
+}
+
+// Función para verificar si el último caracter es un operador
+function esUltimoCaracterOperador() {
+    const operadores = ['+', '-', '*', '/'];
+    const ultimoCaracter = entradaActual.slice(-1);
+    return operadores.includes(ultimoCaracter);
 }
 
 // Función para actualizar el display
